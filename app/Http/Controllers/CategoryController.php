9bde71f6c5b09722c5808d274ff940ed276b329c
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-11 02:23:14 -0700

    Added the create and store function in the CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -26,7 +26,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
@@ -37,7 +37,11 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Category::create([
+            'categoryName' => $request->input('name')
+        ]);
+
+        return redirect()->route('categories.index');
     }
 
     /**
