dfb94e10bb14593c5c9af65f2ed556272e4dd907
dvxgit-tbautista <110645703+dvxgit-tbautista@users.noreply.github.com>
2022-08-11 01:41:14 -0700

    committed some controllers and web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +24,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::resource('categories', CategoryController::class);
+
+Route::resource('posts', PostController::class);
